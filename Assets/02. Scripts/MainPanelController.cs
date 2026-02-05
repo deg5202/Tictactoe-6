d22e8919fe05335c1bc8a63ac9d5d5a9851d21fd
DongHyung <deg5202@gmail.com>
2026-02-05 15:57:30 +0900

    게임매니저, 싱글톤 코드 작성 및 UI 추가 정리

---
+++ Assets/02. Scripts/MainPanelController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainPanelController : MonoBehaviour
+{
+    [SerializeField] private Button singleplayBtn;
+    [SerializeField] private Button dualplayBtn;
+    [SerializeField] private Button settingBtn;
+    
+    public void OnClickSinglePlayButton()
+    {
+        
+    }
+    public void OnClickDualPlayButton()
+    {
+        
+    }
+    public void OnClickSettingButton()
+    {
+        
+    }
+}
